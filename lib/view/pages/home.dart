28bac609462333f1cb385affe806279150703975
fjs <felipejs2001@protonmail.com>
2024-02-21 17:46:54 -0300

    Estrutura básica da home

--- lib/view/pages/home.dart
+++ lib/view/pages/home.dart
@@ -4,46 +4,53 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget{
   const Home({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Home Page"),
-            ElevatedButton(onPressed: () => {Navigator.pushNamed(context, '/login')}, child: const Text("Ir para Login")),
-            Expanded(
-              child: ListView(
-              children: const [
-                ExpansionTile(
-                  title: Text("Doença 1"),
-                  children: [
-                    Text("Resumo da obesidade aqui pra vcs")
-                  ],
-                ),
-                ExpansionTile(
-                  title: Text("Doença 2"),
-                  children: [
-                    Text("Resumo da obesidade aqui pra vcs")
-                  ],
-                ),
-                ExpansionTile(
-                  title: Text("Doença 3"),
-                  children: [
-                    Text("Resumo da obesidade aqui pra vcs")
-                  ],
-                ),
-              ],
-            ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
+//TODO estilizar barra superior
+//TODO criar barra de pesquisa
+//TODO menu lateral
+//TODO estilizar corez
+//TODO rotas para navegar para a página de detalhes
 
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar( 
+
+      title: const Text("Home"),
+    ),
+    body: Column(
+      children: [
+        ElevatedButton(
+          onPressed: () => {Navigator.pushNamed(context, '/login')},
+          child: Text("Ir para Login"), //WARN esse botão é apenas para teste,
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ExpansionTile(
+                  title: Text("Doença $index"),
+                  initiallyExpanded: true,
+                  maintainState: true,
+                  expandedAlignment: Alignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Descrição da Doença $index"),
+                    ),
+                  ],
+                  backgroundColor: const Color.fromARGB(255, 153, 164, 170),
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  collapsedShape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    ),
+  );
+}
 }
