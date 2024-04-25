7a9221f823c0c57c36cc3978c2c73f2b60d47c2f
AndreAnimator <98904347+AndreAnimator@users.noreply.github.com>
2024-04-24 21:26:33 -0300

    Nome da página no drawer

--- lib/view/components/widgets/drawer.dart
+++ lib/view/components/widgets/drawer.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../colors/colors.dart';
 
 //TODO criar backdropfilter para efeito blur
 class MyDrawer extends StatelessWidget {
@@ -10,6 +13,22 @@ class MyDrawer extends StatelessWidget {
   child: ListView(
     padding: EdgeInsets.zero,
     children: [
+      Align(
+        alignment: Alignment(0.7, -0.5),
+      ),
+      const Icon(
+        Icons.arrow_back_rounded,
+        color: AppColors.colorDarkBlue,
+        size: 30.0,
+        semanticLabel: 'Return to previous page',
+      ),
+      Text(
+        ModalRoute.of(context)!.settings.name!,
+        style: GoogleFonts.lora(
+                                  textStyle: const TextStyle(
+                                      color: AppColors.colorDarkBlue),
+                                  fontSize: 30)
+      ),
       ListTile(
         title: const Text('Item 1'),
         onTap: () {
