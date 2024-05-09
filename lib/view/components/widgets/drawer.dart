0bdc22bf13c414b859d8f82ccb42daa24073a48a
AndreAnimator <98904347+AndreAnimator@users.noreply.github.com>
2024-05-09 13:45:30 -0300

    botão de retornar de drawer funcional

--- lib/view/components/widgets/drawer.dart
+++ lib/view/components/widgets/drawer.dart
@@ -10,39 +10,42 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-  child: ListView(
-    padding: EdgeInsets.zero,
-    children: [
-      Align(
-        alignment: Alignment(0.7, -0.5),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(onPressed: () => {Navigator.pop(context)}, icon: const Icon(
+                Icons.arrow_back_rounded,
+                color: AppColors.colorDarkBlue,
+                size: 30.0,
+                semanticLabel: 'Return to previous',
+              ),),
+              Expanded( // Usa Expanded para o texto
+                child: Text(
+                  ModalRoute.of(context)!.settings.name!,
+                  textAlign: TextAlign.center, // Centraliza o texto
+                  style: GoogleFonts.lora(
+                    textStyle: const TextStyle(
+                      color: AppColors.colorDarkBlue,
+                    ),
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          ListTile(
+            title: const Text('Item 1'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {},
+          ),
+        ],
       ),
-      const Icon(
-        Icons.arrow_back_rounded,
-        color: AppColors.colorDarkBlue,
-        size: 30.0,
-        semanticLabel: 'Return to previous page',
-      ),
-      Text(
-        ModalRoute.of(context)!.settings.name!,
-        style: GoogleFonts.lora(
-                                  textStyle: const TextStyle(
-                                      color: AppColors.colorDarkBlue),
-                                  fontSize: 30)
-      ),
-      ListTile(
-        title: const Text('Item 1'),
-        onTap: () {
-        },
-      ),
-      ListTile(
-        title: const Text('Item 2'),
-        onTap: () {
-        },
-      ),
-    ],
-  ),
-);    
-
-
+    );
   }
 }
